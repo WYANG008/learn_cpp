e3817a71ee40048108063f8e00ee6dc65cc41df3
WYANG008 <weitao.yang@finbook.co>
2020-10-10 23:57:14 +0800

    add tests

--- Example.cpp
+++ Example.cpp
@@ -1,6 +1,42 @@
 #include "Example.hpp"
 
+#include <string>
+
 int MAC(int x, int y, int& sum) {
     sum += x * y;
     return sum; 
+}
+
+char* EncodeVarint32(char* dst, uint32_t v) {
+  // Operate on characters as unsigneds
+  uint8_t* ptr = reinterpret_cast<uint8_t*>(dst);
+  static const int B = 128;
+  if (v < (1 << 7)) {
+    *(ptr++) = v;
+  } else if (v < (1 << 14)) {
+    *(ptr++) = v | B;
+    *(ptr++) = v >> 7;
+  } else if (v < (1 << 21)) {
+    *(ptr++) = v | B;
+    *(ptr++) = (v >> 7) | B;
+    *(ptr++) = v >> 14;
+  } else if (v < (1 << 28)) {
+    *(ptr++) = v | B;
+    *(ptr++) = (v >> 7) | B;
+    *(ptr++) = (v >> 14) | B;
+    *(ptr++) = v >> 21;
+  } else {
+    *(ptr++) = v | B;
+    *(ptr++) = (v >> 7) | B;
+    *(ptr++) = (v >> 14) | B;
+    *(ptr++) = (v >> 21) | B;
+    *(ptr++) = v >> 28;
+  }
+  return reinterpret_cast<char*>(ptr);
+}
+
+void PutVarint32(std::string* dst, uint32_t v) {
+  char buf[5];
+  char* ptr = EncodeVarint32(buf, v);
+  dst->append(buf, ptr - buf);
 }
--- test/ExampleTests.cpp
+++ test/ExampleTests.cpp
@@ -1,19 +1,32 @@
 #include <gtest/gtest.h>
+
 #include "../Example.hpp"
 
-TEST(ExampleTests, MAC) {
+// TEST(ExampleTests, MAC) {
    
-    // EXPECT_TRUE(false);
-    // ASSERT_TRUE(false);
+//     // EXPECT_TRUE(false);
+//     // ASSERT_TRUE(false);
 
-    int a = 1, b = 2, sum = 3;
-    int expectedSum = sum + a * b;
-    EXPECT_EQ(
-        expectedSum,
-        MAC(a,b,sum)
-    ) << "MAC test passed !";
-}
+//     int a = 1, b = 2, sum = 3;
+//     int expectedSum = sum + a * b;
+//     EXPECT_EQ(
+//         expectedSum,
+//         MAC(a,b,sum)
+//     ) << "MAC test passed !";
+// }
 
-TEST(ExampleTests2, Square) {
-    EXPECT_EQ(true, true);
+TEST(VariantTests, PutVarint32) {
+    uint32_t v = 111;
+    std::string* s = new std::string("abc");
+    PutVarint32(s, v);
+    EXPECT_EQ(*s, std::string("abco")); // '0' is 111 in ASCII
+
+    std::string* s1 = new std::string("abc");
+    v = 1 << 8;
+    // 10000000 00000010
+    PutVarint32(s1, v);
+    EXPECT_EQ(*s1, std::string("abco"));
+    
+    char* name = "hello";
+    name[2] = 'a';
 }
